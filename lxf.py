06e5628ec8656fb1be551db3205866a408519f21
xixifu <362108178@qq.com>
2021-12-04 21:05:59 +0800

    尝试保存到txt，但with as无法保存列表

--- lxf.py
+++ lxf.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import bs4
+import os
 
 def getHtmlText(url):
     header={"user-agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36"}
@@ -16,26 +17,31 @@ def findHtmlUrl(html,ulist):
         ulist[name.string] = "https://www.liaoxuefeng.com"+name["href"]
 
 
-def findHtmlText(html):
+def findHtmlText(html,text):
     soup = BeautifulSoup(html,"lxml")
     lis = soup.select("div.x-wiki-content.x-main-content p")
     for li in lis :
         if li.string != None:
-            print(li.string)
+            text.append(li.string)
 
-def Downloadfild(text,flidPash):
-    pass
+def Downloadfild(t,findPash):
+    with open(findPash,"a",encoding='utf-8') as f:
+        f.write(text)   
+        f.write("\n")
 
 
 if __name__ == "__main__":
     ulist = {}
+    text = []
+    findPash = "./lxf_text.txt"
     url = "https://www.liaoxuefeng.com/wiki/896043488029600"
     html = getHtmlText(url)
     findHtmlUrl(html,ulist)
     for name,urll in ulist.items():
         htmll = getHtmlText(urll)
-        findHtmlText(htmll)
-        print("\n")
+        findHtmlText(htmll,text)
+        for t in text:
+            Downloadfild(t,findPash)
 
 
 
