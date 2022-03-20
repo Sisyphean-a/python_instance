e5cfe9b8ffd69f32f3af02834e0da4b3eb08b660
Sisyphean-a <362108178@qq.com>
2022-03-20 11:47:03 +0800

    无

--- text.py
+++ text.py
@@ -1,7 +1,25 @@
 import pandas as pd 
+import matplotlib.pyplot as plt
 
 url = "https://info.usd-cny.com/wti/lishi.htm"
 
 table = pd.read_html(url)
+df = pd.DataFrame(table[0]).set_index("年份")
 
-print(table)
+del_head = lambda x : float(x[1:])
+del_tail = lambda x : float(x[:-1])
+
+df["最低价"] = df["最低价"].apply(del_head)
+df["平均价"] = df["平均价"].apply(del_head)
+df["最高价"] = df["最高价"].apply(del_head)
+df["年幅度"] = df["年幅度"].apply(del_tail)
+
+amplitude_location = lambda x : x>0
+location_mean_value = lambda x : 80>x>=50
+
+df = df.loc[df.年幅度.apply(amplitude_location)] \
+      .loc[df["平均价"].apply(location_mean_value)]
+
+print(df)
+
+#print(type(df["年幅度"][1]))
