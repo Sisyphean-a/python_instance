9f1b02b9c84bd5f3172d59fcf2510b830544e2bc
Sisyphean-a <362108178@qq.com>
2022-03-25 20:44:31 +0800

    无

---
+++ 46.py
@@ -0,0 +1,22 @@
+from lib2to3.pgen2 import driver
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+
+url="https://passport.neea.edu.cn/CETLogin?ReturnUrl=https://cet-bm.neea.edu.cn/Home/VerifyPassport/?LoginType=0&Safe=1"
+
+url2="https://cet-bm.neea.edu.cn/Student/Index"
+
+url3="https://cet-bm.neea.edu.cn/Student/DetailsQC?r=0.791422575684413"
+
+url4="https://xds.ljxfw.gov.cn/enrollSix/views/login.html"
+
+driver.get(url4)
+
+driver.find
+
+'''
+412727199802204097
+夏坤明
+
+'''
--- text.py
+++ text.py
@@ -1,12 +1,10 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-#plt.rcParams['font.sans-serif'] = ['Microsoft YaHei'] 
-
 url = "https://info.usd-cny.com/wti/lishi.htm"
 
 table = pd.read_html(url)
-df = pd.DataFrame(table[0])
+df = pd.DataFrame(table[0]).set_index("年份")
 
 del_head = lambda x : float(x[1:])
 del_tail = lambda x : float(x[:-1])
@@ -16,21 +14,18 @@ df["平均价"] = df["平均价"].apply(del_head)
 df["最高价"] = df["最高价"].apply(del_head)
 df["年幅度"] = df["年幅度"].apply(del_tail)
 
-print(df.head())
+df.sort_values(by="平均价",inplace=True,ascending=False)
 
-df.plot(y="平均价",
+
+df.plot(y=["平均价","最高价"],
     kind="bar",
-    #color="red",
+    color=["red","orange"],
     title="US油价",
-    rot=30)
+    rot=60)
+
+plt.title("us油价",fontsize=18,fontweight="bold")
+
+plt.xlabel("年份",fontweight="bold")
+plt.ylabel("价格",fontweight="bold")
+
 plt.show()
-'''
-AR PL UKai CN
-AR PL Mingti2L Big5
-Unifont
-Noto Sans CJK JP
-AR PL KaitiM Big5
-Noto Serif CJK JP
-Droid Sans Fallback
-AR PL UMing CN
-'''
